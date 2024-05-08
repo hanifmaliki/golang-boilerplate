37f8c44efc068cea7a31a249061d2237b76d3e2d
Hanif Maliki Dewanto <Hanif.Dewanto@petrosea.com>
2024-05-08 15:17:02 +0700

    Repo

--- internal/pkg/repository/address_repository.go
+++ internal/pkg/repository/address_repository.go
@@ -1,1 +1,29 @@
 package repository
+
+import (
+	"github.com/hanifmaliki/go-boilerplate/internal/pkg/entity"
+
+	"gorm.io/gorm"
+)
+
+type AddressRepository interface {
+	FindByToken(user *entity.User, token string) error
+}
+
+type addressRepository struct {
+	BaseRepository[entity.User]
+}
+
+func NewAddressRepository(db *gorm.DB) AddressRepository {
+	r := &addressRepository{}
+	r.db = db
+	return r
+}
+
+func (r *addressRepository) FindByToken(user *entity.User, token string) error {
+	return r.db.Where("token = ?", token).First(user).Error
+}
+
+func (r *addressRepository) CreateWithAddressCreditCard(user *entity.User, token string) error {
+	return nil
+}
--- internal/pkg/repository/repository.go
+++ internal/pkg/repository/repository.go
@@ -18,7 +18,13 @@ func NewRepository(db *gorm.DB) Repository {
 }
 
 func (r *repository) Transaction(fn func(r Repository) error) error {
-	return nil
+	err := r.db.Transaction(func(tx *gorm.DB) error {
+		newRepository := NewRepository(tx)
+		err := fn(newRepository)
+		return err
+	})
+
+	return err
 }
 
 func (r *repository) User() UserRepository {
@@ -29,24 +35,24 @@ type BaseRepository[T any] struct {
 	db *gorm.DB
 }
 
-func (r *BaseRepository[T]) Create(db *gorm.DB, entity *T) error {
-	return db.Create(entity).Error
+func (r *BaseRepository[T]) Create(entity *T) error {
+	return r.db.Create(entity).Error
 }
 
-func (r *BaseRepository[T]) Update(db *gorm.DB, entity *T) error {
-	return db.Save(entity).Error
+func (r *BaseRepository[T]) Update(entity *T) error {
+	return r.db.Save(entity).Error
 }
 
-func (r *BaseRepository[T]) Delete(db *gorm.DB, entity *T) error {
-	return db.Delete(entity).Error
+func (r *BaseRepository[T]) Delete(entity *T) error {
+	return r.db.Delete(entity).Error
 }
 
-func (r *BaseRepository[T]) CountById(db *gorm.DB, id any) (int64, error) {
+func (r *BaseRepository[T]) CountById(id any) (int64, error) {
 	var total int64
-	err := db.Model(new(T)).Where("id = ?", id).Count(&total).Error
+	err := r.db.Model(new(T)).Where("id = ?", id).Count(&total).Error
 	return total, err
 }
 
-func (r *BaseRepository[T]) FindById(db *gorm.DB, entity *T, id any) error {
-	return db.Where("id = ?", id).Take(entity).Error
+func (r *BaseRepository[T]) FindById(entity *T, id any) error {
+	return r.db.Where("id = ?", id).Take(entity).Error
 }
