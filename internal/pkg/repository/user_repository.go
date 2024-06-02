2b9c4e7f208f84a5909ddbb537cb546f3a9a103c
Hanif Maliki Dewanto <Hanif.Dewanto@petrosea.com>
2024-06-02 21:22:29 +0700

    Fix find

--- internal/pkg/repository/user_repository.go
+++ internal/pkg/repository/user_repository.go
@@ -29,9 +29,8 @@ func NewUserRepository(db *gorm.DB) UserRepository {
 func (r *userRepository) Find(ctx context.Context, request *model.GetUserRequest, query *pkg_model.Query) ([]*entity.User, *pkg_model.Pagination, error) {
 	var datas []*entity.User
 	db := r.db.WithContext(ctx)
-	baseQuery := db
 
-	// Handle filter
+	// Apply filters
 	if request.Search != "" {
 		search := "%" + request.Search + "%"
 		db = db.Where("name LIKE ? OR email LIKE ?", search, search)
@@ -40,48 +39,44 @@ func (r *userRepository) Find(ctx context.Context, request *model.GetUserRequest
 		db = db.Where("company_id = ?", request.CompanyID)
 	}
 	if len(request.RoleID) > 0 {
-		db = db.Where("id IN (?)", baseQuery.Table("user_roles").Select("user_id").
+		db = db.Where("id IN (?)", r.db.Table("user_roles").Select("user_id").
 			Where("role_id IN ?", request.RoleID))
 	}
 
 	// New Session Methods for handle contaminated SQL queries
 	db = db.Session(&gorm.Session{})
 
-	// Initialize the main query
-	mainQuery := db
-
 	// Pagination setup
 	var pagination *pkg_model.Pagination
 	if query.Page > 0 && query.PageSize > 0 {
-		pagination = &pkg_model.Pagination{
-			Page:     query.Page,
-			PageSize: query.PageSize,
-		}
-
 		var totalItems int64
 		err := db.Model(&entity.User{}).Count(&totalItems).Error
 		if err != nil {
 			return nil, nil, err
 		}
-		pagination.TotalItems = totalItems
-		pagination.TotalPages = int(math.Ceil(float64(totalItems) / float64(query.PageSize)))
 
-		mainQuery = mainQuery.Offset((query.Page - 1) * query.PageSize).Limit(query.PageSize)
+		pagination = &pkg_model.Pagination{
+			Page:       query.Page,
+			PageSize:   query.PageSize,
+			TotalItems: totalItems,
+			TotalPages: int(math.Ceil(float64(totalItems) / float64(query.PageSize))),
+		}
+
+		db = db.Offset((query.Page - 1) * query.PageSize).Limit(query.PageSize)
 	}
 
 	// Handle preload/expansion of related entities
 	for _, expand := range query.Expand {
-		mainQuery = mainQuery.Preload(expand)
+		db = db.Preload(expand)
 	}
 
 	// Handle sorting
 	if query.SortBy != "" {
-		mainQuery = mainQuery.Order(query.SortBy)
+		db = db.Order(query.SortBy)
 	}
 
 	// Execute the query
-	err := mainQuery.Find(&datas).Error
-	if err != nil {
+	if err := db.Find(&datas).Error; err != nil {
 		return nil, nil, err
 	}
 
